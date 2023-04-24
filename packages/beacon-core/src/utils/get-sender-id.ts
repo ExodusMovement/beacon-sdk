ca12d79ecb338d991421e102c72333ac6ac470cf
Matias Lang <matias.la@exodus.io>
2023-04-24 11:55:49 -0300

    use 128-bit senderId

--- packages/beacon-core/src/utils/get-sender-id.ts
+++ packages/beacon-core/src/utils/get-sender-id.ts
@@ -9,7 +9,7 @@ import * as bs58check from 'bs58check'
  * @param publicKey
  */
 export const getSenderId = async (publicKey: string): Promise<string> => {
-  const buffer = Buffer.from(blake2b(Buffer.from(publicKey, 'hex'), undefined, 5))
+  const buffer = Buffer.from(blake2b(Buffer.from(publicKey, 'hex'), undefined, 16))
 
   return bs58check.encode(buffer)
 }
